463e69d0f4aca02696e45cfc24451e57b4228381
Thomas Schmidt <tschmidt@suse.de>
2017-02-23 18:04:56 +0100

    initial setup for searching users

---
+++ src/App.vue
@@ -0,0 +1,119 @@
+<template>
+  <div id="app">
+
+    <header class="header grid grid-pad">
+      <div class="col-7-12">
+        <img width="540" height="100" src="./assets/suse_geekos.png" alt="Suse geekos"/>
+      </div>
+      <div class="col-5-12">
+        <ul class="nav">
+          <li class="active"><a href="/search">Search</a></li>
+          <li>Teams</li>
+          <li><a href="http://floor.suse.de">Offices</a></li>
+        </ul>
+      </div>
+
+    </header>
+
+
+    <div class="content grid grid-pad">
+      <div class="col-1-1">
+
+        <router-view></router-view>
+
+      </div>
+    </div>
+
+
+    <footer class="footer grid grid-pad">
+      <div class="col-1-1">
+        SUSE Geeko Browser. <a href="https://hackweek.suse.com/15/projects/1868">Hackweek project</a>.
+        Please report issues and feature requests on <a href="https://github.com/SUSE/HiGecko/issues">Github</a>.
+      </div>
+    </footer>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style>
+  /* https://thisisdallas.github.io/Simple-Grid/
+  @import "simplegrid.css";
+
+  $suse-green: #02d35f;
+  $suse-green-light: rgb(160, 255, 95);
+  $suse-dark: rgb(51, 51, 51);
+  $suse-grey-light: #dcdddf;
+  $suse-grey-mid: #6a7c87;   */
+
+  body {
+    background: #fff;
+    font-family: "Open Sans";
+    color: rgb(51, 51, 51);
+    margin: 0;
+    padding: 0;
+  }
+
+  a, a:visited, a:active {
+    text-decoration: none;
+    color: #02d35f;
+  }
+
+  .green {
+    color: #02d35f;
+  }
+
+  header {
+    border-bottom: #02d35f solid 5px;
+    padding-bottom: 15px;
+  }
+
+  header a, header a:visited, header a:active {
+    text-decoration: none;
+    color: rgb(51, 51, 51);
+  }
+
+  .nav {
+    list-style: none;
+    text-align: center;
+    padding: 0;
+    margin: 0;
+    position: relative;
+    top: 80px;
+    left: 107px;
+  }
+
+  .nav li {
+    font-size: 1.2em;
+    line-height: 40px;
+    height: 40px;
+    width: 120px;
+    float: left;
+  }
+
+  .nav a {
+    display: block;
+    transition: .3s background-color;
+  }
+
+  .nav a:hover {
+    background-color: #02d35f;
+  }
+
+  .nav .active {
+    background-color: #02d35f;
+  }
+
+  footer {
+    text-align: center;
+    border-top: #02d35f solid 5px;
+    padding-top: 20px;
+    margin-top: 30px !important;
+    margin-bottom: 20px !important;
+  }
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,170 @@
+<template>
+
+  <div id="search">
+
+    <h2>Geeko Search: Find your colleagues!</h2>
+    <input id="search-input" class="search" v-on:keyup.enter="search" v-model.trim="search_input" placeholder="What are you searching for?" autofocus>
+
+    <div class="search-results-info" v-if="search_results.length">{{search_results.length}} results found</div>
+
+    <div class="search-results">
+      <template v-for="result in search_results">
+
+        <div class="flip-container search-result" ontouchstart="this.classList.toggle('hover');">
+          <div class="flipper">
+            <div class="front">
+              <img class="result-image lazyload" v-bind:data-src="result.picture" width="160">
+
+              <div class="result-content">
+                <b>{{ result.fullname }}</b>
+              </div>
+            </div>
+            <div class="back">
+              <div class="result-content">
+                <h4>{{ result.fullname }}</h4>
+                <p><i>{{ result.title }}</i></p>
+                <p v-if="result.email"><a v-bind:href="'mailto:' + result.email"><i class="fa fa-envelope-o fa-lg green"></i></a></p>
+                <p v-if="result.phone"><i class="fa fa-phone fa-lg green"></i> {{ result.phone }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </template>
+    </div>
+
+
+  </div>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'search',
+    data () {
+      return {
+        search_input: '',
+        search_results: []
+      }
+    },
+    methods: {
+      search: function () {
+        // var search_icon = $("#search-input").css('background-image')
+        // $("#search-input").css('background-image', "url(/images/ajax-loader.gif)")
+        var search = this
+        this.search_results = []
+        // https://github.com/mzabriskie/axios
+        this.axios.get('http://localhost:3000/api/search', {
+          params: {
+            q: this.search_input
+          }
+        })
+          .then(function (response) {
+            console.log(response.data.search.results)
+            search.search_results = response.data.search.results
+            // $("#search-input").css('background-image', search_icon)
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+      }
+    }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  #search {
+    text-align: center;
+  }
+
+  #search-input {
+    width: 430px;
+    box-sizing: border-box;
+    border: 2px solid #dcdddf;
+    border-radius: 4px;
+    font-size: 16px;
+    background-color: white;
+    background-image: image-url('icons/search.png');
+    background-position: 10px 12px;
+    background-repeat: no-repeat;
+    padding: 12px 20px 12px 40px;
+  }
+
+  .search-results-info {
+    margin-top: 5px;
+    color: #6a7c87;
+  }
+
+  .search-results {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  .search-result {
+    margin: auto;
+    margin: 1em 2em 1em 3em;
+    border: 1px solid #ccc;
+    margin: 15px;
+    border-top: #02d35f solid 5px;
+    border-bottom: #02d35f solid 5px;
+    float: left;
+    width: 160px;
+  }
+
+  .result-image {
+  }
+
+  .result-content {
+    padding: 10px;
+  }
+
+  /* card flipping */
+
+  .flip-container {
+    perspective: 1000px;
+  }
+
+  /* flip the pane when hovered */
+  .flip-container:hover .flipper, .flip-container.hover .flipper {
+    transform: rotateY(180deg);
+  }
+
+  .flip-container, .front, .back {
+    height: 250px;
+    width: 160px;
+  }
+
+  /* flip speed goes here */
+  .flipper {
+    transition: 0.6s;
+    transform-style: preserve-3d;
+    position: relative;
+  }
+
+  /* hide back of pane during swap */
+  .front, .back {
+    backface-visibility: hidden;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  /* front pane, placed above back */
+  .front {
+    z-index: 2;
+    /* for firefox 31 */
+    transform: rotateY(0deg);
+  }
+
+  /* back, initially hidden pane */
+  .back {
+    transform: rotateY(180deg);
+  }
+
+</style>
